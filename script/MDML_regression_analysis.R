da50f33748bb22a6486521dec60ad95a62aaaad3
Corinne Brenner <cjb399@nyu.edu>
2019-12-10 12:51:19 -0500

    adding pkgs

--- script/MDML_regression_analysis.R
+++ script/MDML_regression_analysis.R
@@ -1,9 +1,13 @@
 require(tidyverse)
-
+require(ROCR)
+require(ranger)
+require(glmnet)
 
 #For Lasso regression 
 #Read in AYCET gameplay data and DCCS data
 AYCET_gameplay_aggregated <- read_csv("data/AYCET_gameplay_aggregated.csv")
+
+
 
 
 
